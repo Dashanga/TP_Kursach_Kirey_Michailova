da1a14893af685e2d6df9950bc69a4b60e1c9254
My Name <north.pole@list.ru>
2019-05-05 20:35:08 +0400

    1

---
+++ YouAreTerrible/BeautyModel/Provider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautyModel
+{
+    public class Provider
+    {
+        public int ProviderId { get; set; }
+        public string ProviderName { get; set; }
+        public string ProviderSurname { get; set; }
+        public string ProviderPatronymic { get; set; }
+    }
+}
